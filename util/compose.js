66d3a92f0fb9816efe9bdfcef5a72cd9166beb06
lyuns <lyunst@163.com>
2018-11-07 11:04:02 +0800

    compose more function

--- util/compose.js
+++ util/compose.js
@@ -1,6 +1,22 @@
-const compose = (f, g) => x => f(g(x))
+/*
+ * 函数组合
+ */
+// const compose = (f, g) => x => f(g(x))
+
+/*
+ * 多函数组合
+ */
+const composeX = (...args) => {
+    return x => {
+        let prev = x
+        for(let i=args.length-1; i>=0; i--){
+            prev = args[i](prev)
+        }
+        return prev;
+    }
+}
 
 let toUpper = x => x.toUpperCase()
-let addShot = x => x + '.'
+let addShot = x => x + '...'
 
-let upperShot = compose(toUpper, addShot)
+let upperShot = composeX(toUpper, addShot)
